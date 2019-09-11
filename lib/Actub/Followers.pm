7f16f1550d74ec8d934c97ab00a32d9f7ba87390
SHIRAKATA Kentaro <argrath@ub32.org>
2019-09-12 04:09:27 +0900

    remove unused code

--- lib/Actub/Followers.pm
+++ lib/Actub/Followers.pm
@@ -10,15 +10,6 @@ use WWW::ActivityPub::OrderedCollection;
 my @context = (
     "https://www.w3.org/ns/activitystreams",
     "https://w3id.org/security/v1",
-    {
-        manuallyApprovesFollowers => "as:manuallyApprovesFollowers",
-        sensitive => "as:sensitive",
-        hashtag => "as:Hashtag",
-        ostatus => "http://ostatus.org#",
-        atomUri => "ostatus:atomUri",
-        inReplyToAtomUri => "ostatus:inReplyToAtomUri",
-        conversation => "ostatus:conversation",
-    },
     );
 
 sub add {
